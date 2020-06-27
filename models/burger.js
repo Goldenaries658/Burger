5d8df1b2236d908199b56ee57e643f635bb1dffa
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-06-27 23:06:15 +0100

    added async await

--- models/burger.js
+++ models/burger.js
@@ -1,13 +1,30 @@
 const orm = require('../config/orm');
 
 module.exports = {
-  selectAll: () => {
-    orm.selectAll('burgers');
+  selectAll: async () => {
+    try {
+      return await orm.selectAll('burgers');
+    } catch (err) {
+      console.error(`ERROR - burger.js - selectAll(): ${err}`.red.bold);
+    }
   },
-  insertOne: (burgerName) => {
-    orm.insertOne('burgers', burgerName);
+  insertOne: async (burgerName) => {
+    try {
+      return await orm.insertOne('burgers', burgerName);
+    } catch (err) {
+      console.error(`ERROR - orm.js - insertOne(): ${err}`.red.bold);
+    }
   },
-  updateOne: (newBurgerName, burgerID) => {
-    orm.updateOne('burgers', 'burger_name', newBurgerName, burgerID);
+  updateOne: async (newBurgerName, burgerID) => {
+    try {
+      return await orm.updateOne(
+        'burgers',
+        'burger_name',
+        newBurgerName,
+        burgerID
+      );
+    } catch (err) {
+      console.error(`ERROR - orm.js - updateOne(): ${err}`.red.bold);
+    }
   },
 };
