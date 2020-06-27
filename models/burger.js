aefb4227ec96b32ab14a8ae82ecfa92a51347e7b
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-06-27 22:09:39 +0100

    updateOne() method

--- models/burger.js
+++ models/burger.js
@@ -7,4 +7,7 @@ const burger = {
   insertOne: (burgerName) => {
     orm.insertOne('burgers', burgerName);
   },
+  updateOne: (newBurgerName, burgerID) => {
+    orm.updateOne('burgers', 'burger_name', newBurgerName, burgerID);
+  },
 };
