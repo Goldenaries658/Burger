7a63edf4bd5ae35b780d625d45d62560f3cd92ee
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-06-27 22:34:04 +0100

    Setuo express server

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+
+const PORT = process.env.PORT || 3300;
+
+const app = express();
+
+// Location for static content
+app.use(express.static('public'));
+
+// Express middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.listen(PORT, () => console.log(`App now listening at localhost: ${PORT}`));
