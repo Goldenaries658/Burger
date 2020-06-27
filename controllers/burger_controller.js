5ff2e063b8155f04203a1bf6345b287556538afc
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-06-27 23:17:55 +0100

    .get('/api/burger') method

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -5,7 +5,15 @@ const burger = require('../models/burger');
 const router = express.Router();
 
 // API routes
-router.get('/', (req, res) => {
-  const data = burger.selectAll();
-  res.status(200).json(data);
+router.get('/api/burger', async (req, res) => {
+  try {
+    const data = await burger.selectAll();
+    res.status(200).json(data);
+  } catch (err) {
+    console.error(
+      `ERROR - burger_controller.js - .get('/api'): ${err}`.red.bold
+    );
+  }
 });
+
+module.exports = router;
