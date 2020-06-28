e2e9970670ee81875374c56d6ba2d4b546ba6bfa
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-06-28 13:45:26 +0100

    .put('/api/burgers') method, added express error handling middleware

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -5,7 +5,7 @@ const burger = require('../models/burger');
 const router = express.Router();
 
 // API routes
-router.get('/api/burgers', async (req, res) => {
+router.get('/api/burgers', async (req, res, next) => {
   try {
     const data = await burger.selectAll();
     res.status(200).json(data);
@@ -13,10 +13,11 @@ router.get('/api/burgers', async (req, res) => {
     console.error(
       `ERROR - burger_controller.js - .get('/api/burgers'): ${err}`.red.bold
     );
+    next();
   }
 });
 
-router.post('/api/burgers', async (req, res) => {
+router.post('/api/burgers', async (req, res, next) => {
   const burgerName = req.body.name;
   try {
     await burger.insertOne(burgerName);
@@ -25,6 +26,27 @@ router.post('/api/burgers', async (req, res) => {
     console.error(
       `ERROR - burger_controller.js - .post('/api/burgers'): ${err}`.red.bold
     );
+    next(err);
+  }
+});
+
+router.put('/api/burgers/:id', async (req, res, next) => {
+  const burgerId = req.params.id;
+  const newBurgerName = req.body.name;
+  console.log(newBurgerName, burgerId);
+
+  try {
+    const result = await burger.updateOne(newBurgerName, burgerId);
+    console.log(result);
+    if (result.changedRows === 0) {
+      throw new Error('0 Rows Changed: An unknown error occurred.');
+    }
+    res.sendStatus(200);
+  } catch (err) {
+    console.error(
+      `ERROR - burger_controller.js - .put('/api/burgers'): ${err}`.red.bold
+    );
+    next(err);
   }
 });
 
