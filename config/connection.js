225d184457482c660bda7a7bb1d2951c396b3fb9
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-06-30 02:30:27 +0100

    updated env to stop error on heroku

--- config/connection.js
+++ config/connection.js
@@ -6,14 +6,14 @@ const result = dotenv.config();
 
 if (result.error) throw result.error;
 
-const defaultConnection = {
-  host: process.env.DB_HOST,
-  port: 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: 'burgers_db',
-};
-
 module.exports = mysql.createConnection(
-  process.env.JAWSDB_URL ? process.env.JAWSDB_URL : defaultConnection
+  process.env.JAWSDB_URL
+    ? process.env.JAWSDB_URL
+    : {
+        host: process.env.DB_HOST,
+        port: 3306,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: 'burgers_db',
+      }
 );
