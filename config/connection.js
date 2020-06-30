e915b110f473c837635ab46d68751f01834c782f
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-06-30 02:41:06 +0100

    test build 2

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,7 @@ const mysql = require('mysql');
 
 // Setting environment variables
 const dotenv = require('dotenv');
-const result = dotenv.config();
-
-if (result.error) throw result.error;
+dotenv.config();
 
 const defaultConnection = {
   host: process.env.DB_HOST,
@@ -16,12 +14,12 @@ const defaultConnection = {
 
 module.exports = mysql.createConnection(
   process.env.JAWSDB_URL
-  // ? process.env.JAWSDB_URL
-  // : {
-  //     host: process.env.DB_HOST,
-  //     port: 3306,
-  //     user: process.env.DB_USER,
-  //     password: process.env.DB_PASS,
-  //     database: 'burgers_db',
-  // }
+    ? process.env.JAWSDB_URL
+    : {
+        host: process.env.DB_HOST,
+        port: 3306,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: 'burgers_db',
+      }
 );
