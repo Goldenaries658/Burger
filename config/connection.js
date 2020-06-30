a8ce6b635ffb881380ea623460445f732fbb61e2
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-06-30 01:23:24 +0100

    added env values

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,19 @@
 const mysql = require('mysql');
 
+// Setting environment variables
+const dotenv = require('dotenv');
+const result = dotenv.config();
+
+if (result.error) throw result.error;
+
 const defaultConnection = {
-  host: 'localhost',
+  host: process.env.DB_HOST,
   port: 3306,
-  user: 'root',
-  password: '',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
   database: 'burgers_db',
 };
 
 module.exports = mysql.createConnection(
   process.env.JAWSDB_URL ? process.env.JAWSDB_URL : defaultConnection
 );
-
-
